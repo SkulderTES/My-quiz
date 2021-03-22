5797abdec69a6528dd34c73e450d91945d0c4f14
SkulderTES <75627386+SkulderTES@users.noreply.github.com>
2021-03-22 18:04:15 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+var canvas;
+var gameState = 0;
+var contestantCount,allContestants,answer,database,question,contestant,quiz;
+function setup(){
+  canvas = createCanvas(850,400);
+  database = firebase.database();
+  quiz = new Quiz();
+  quiz.getState();
+  quiz.start();
+}
+
+
+function draw(){
+  background("pink");
+
+  if (contestantCount === 4){
+    quiz.update(1);
+  }
+  if (gameState === 1){
+  quiz.play();
+  }
+}
